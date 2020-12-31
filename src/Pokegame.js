478c389fcfa787c7cf9817e4df379a08e3fca53b
Mengmei Tu <redmmeii@gmail.com>
2020-12-30 21:14:51 -0600

    update styling

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -31,7 +31,7 @@ class Pokegame extends Component {
         return (
             <div>
                 <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
-                <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp2} />
+                <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
             </div>
         );
     }
